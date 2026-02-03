fd748707840d00cd29eccd38d800a361bb4f3dc7
Cortexd <Cortexd@hotmail.com>
2026-02-03 12:47:02 +0100

    Ajout log + 1er test OK

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import logging
 import time
 import smtplib
 import sqlite3
@@ -6,12 +7,27 @@ from telegram import Bot
 from telegram.ext import Updater, CommandHandler
 from dotenv import load_dotenv
 
+# Configurer le logging
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    handlers=[
+        #logging.FileHandler("app.log"),
+        logging.StreamHandler()
+    ]
+)
+
+logger = logging.getLogger(__name__)
+
+
 # Configuration
 load_dotenv()  # Charge les variables d'environnement à partir du fichier .env
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 EMAIL_ADDRESS = os.getenv("EMAIL_ADDRESS")
 EMAIL_PASSWORD = os.getenv("EMAIL_PASSWORD")
-NOTIFICATION_INTERVAL = 604800  # 7 jours en secondes
+YOUR_CHAT_ID = os.getenv("YOUR_CHAT_ID")
+#NOTIFICATION_INTERVAL = 604800  # 7 jours en secondes
+NOTIFICATION_INTERVAL = 30  
 
 # Database setup (optional)
 def setup_database():
@@ -28,11 +44,17 @@ def setup_database():
 
 # Function to send Telegram message
 def send_telegram_message(chat_id, message):
-    bot = Bot(token=TELEGRAM_TOKEN)
-    bot.send_message(chat_id=chat_id, text=message)
+    logger.info(f"Sending message to {chat_id}: {message}")
+    try:
+        bot = Bot(token=TELEGRAM_TOKEN)
+        bot.send_message(chat_id=chat_id, text=message)
+        logger.info(f"Message sent successfully to {chat_id}")
+    except Exception as e:
+        logger.error(f"Error sending message to {chat_id}: {e}")
 
 # Function to send email
 def send_email(recipient, subject, body):
+    logger.info(f"Sending message to {chat_id}: {message}")
     with smtplib.SMTP('smtp.gmail.com', 587) as server:
         server.starttls()
         server.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
@@ -42,9 +64,10 @@ def send_email(recipient, subject, body):
 def stop_notification(update, context):
     update.message.reply_text("Merci, vous avez confirmé que tout va bien.")
     # Update the database status here (e.g., set responded to 1)
-    
+
 # Main application loop
 def main():
+    logger.info("Starting the application...")
     setup_database()
     updater = Updater(token=TELEGRAM_TOKEN, use_context=True)
     dispatcher = updater.dispatcher
